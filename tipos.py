02d8868d5e5b0a4af453af0b52110e0b1b877253
Francisco Mora <fr.morac@duocuc.cl>
2023-03-02 17:54:05 -0300

    Add tipos.py

---
+++ tipos.py
@@ -0,0 +1,20 @@
+# string
+my_name = "fmoradev"
+my_name = 'Pepe'
+print('my_name =>', my_name)
+print(type(my_name))
+
+#int
+my_age = 36
+print(my_age)
+print(type(my_age))
+
+# boolean T o F mayuscula
+is_single = True
+print('is_single => ', is_single)
+print(type(is_single))
+
+# inputs
+my_age = input('Cual es tu edad?')
+print('my_age =>', my_age)
+print(type(my_age))
