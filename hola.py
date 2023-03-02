2181c8ce134a8b95bae720bbe4068bf51755e23c
Francisco Mora <fr.morac@duocuc.cl>
2023-03-02 11:46:53 -0300

    first commit

---
+++ hola.py
@@ -0,0 +1,8 @@
+print("Hola, esto es python")
+print("Hola soy fmoradev y tengo n años")
+print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+
+# comentario
---
+++ variables.py
@@ -0,0 +1,15 @@
+print("Hola, Variables")
+
+# Declarando variables
+my_name = "fmoradev"
+print(my_name)
+
+my_age = 18
+print(my_age)
+
+my_name = "Pedro"
+print("Aquí cambié mi nombre a:", my_name)
+
+# pido un valor al usuario
+my_name = input("Ingresa tu nombre:")
+print("Usando input:", my_name)
