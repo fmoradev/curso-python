37b8150156e2da3a3e037155665b388636697a06
Francisco Mora <fr.morac@duocuc.cl>
2023-03-06 16:57:32 -0300

    Add transformacion.py

---
+++ transformacion.py
@@ -0,0 +1,18 @@
+name = "Pedro"
+print(type(name))
+
+name = 12
+print(type(name))
+
+name = True
+print(type(name))
+
+age = 12
+print("Mi edad es: " + str(age))
+print(f"Mi edad es: {age}")
+
+age = input("Tu edad:")
+print(type(age))
+age = int(age)
+age += 10
+print(f"Tu edad en 10 años sera: {age}")
