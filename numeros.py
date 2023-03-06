0fe0cded959502d2565b42f913909b0bbec21013
Francisco Mora <fr.morac@duocuc.cl>
2023-03-06 16:36:13 -0300

    Add numeros.py

---
+++ numeros.py
@@ -0,0 +1,19 @@
+lives = 3
+print(type(lives))
+
+age = 12
+budget = 100
+
+# flotantes
+temperature = 12.4
+print(type(temperature))
+
+lives = 2
+print(lives)
+lives = 1
+print(lives)
+lives = 12 + 15
+print(lives)
+
+lives = lives - 1
+print(lives)
