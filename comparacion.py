8102f1d6e6fd0220849a82829a97e263bd315d2f
Francisco Mora <fr.morac@duocuc.cl>
2023-03-07 14:40:47 -0300

    Add comparacion.py

---
+++ comparacion.py
@@ -0,0 +1,34 @@
+# >
+print(7 > 3)
+print(3 > 7)
+print(7 > 7)
+
+# <
+print(3 < 7)
+print(7 < 3)
+print(7 < 7)
+
+# >=
+print(2 >= 1)
+print(2 >= 3)
+print(2 >= 2)
+
+# <=
+print(1 <= 2)
+print(2 <= 1)
+print(2 <= 2)
+
+# ==
+print(6 == 6)
+print(5 == 2)
+
+# !=
+print(6 != 10)
+print(6 != 6)
+
+# strings
+print("Apple" == 'Apple') #True
+print("Apple" == 'apple') #False
+print("1" == 1) #False
+age = 17
+print(age >= 18)
